2208f218c42b3fb6bc71c4a781f0a852fcff8a7e
Howiemorg <morgenthalerhowie@yahoo.com>
2023-05-28 17:41:26 -0500

    created modal for cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,26 @@
-import { Fragment } from "react";
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
-          <div className={classes.total}>
-              <span>Total Amount</span>
-              <span>35.62</span>
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
       </div>
-          <div className={classes.actions}>
-              <button className={classes['button--alt']}></button>
-              <button className={classes.button}>Order</button>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
