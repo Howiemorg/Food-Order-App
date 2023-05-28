4e207da7c7bd2b503f860890c07c17ca92a9e073
Howiemorg <morgenthalerhowie@yahoo.com>
2023-05-28 14:30:55 -0500

    Added header and brought the image to it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Fragment } from "react";
+
+import Header from "./components/Layout/Header";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <h1></h1>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
