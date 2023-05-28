fe19cf879b0792e1555c87800416e6d2039782a0
Howiemorg <morgenthalerhowie@yahoo.com>
2023-05-28 18:22:24 -0500

    make the cart showable and hidable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartShown, setCartShown] = useState(false);
+
+  const showCart = () => {
+    setCartShown(true);
+  };
+
+  const hideCart = () => {
+    setCartShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartShown && <Cart onHideCart={hideCart} />}
+      <Header onShowCart={showCart} />
       <main>
         <Meals />
       </main>
