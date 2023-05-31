8231e12c7e0493a8e54ec69d1a9c540985a61c44
Howiemorg <morgenthalerhowie@yahoo.com>
2023-05-30 19:17:38 -0500

    added timer and useEffect for add to cart animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-  const cartCtx = useContext(CartContext);
+  const [animate, setAnimate] = useState(false);
 
-  const cartAmount = cartCtx.items.reduce((currNumber, item) => {
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const cartAmount = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${animate ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setAnimate(true);
+
+    const timer = setTimeout(() => {
+      setAnimate(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
